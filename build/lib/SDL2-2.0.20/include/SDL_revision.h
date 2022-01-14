92ac4d5366788ff3f787fac854b22a7ca78f5cd5
Runtime-Learner <matthias15002@gmail.com>
2022-01-14 16:19:09 -0500

    Add actual SDL2 library that can be added to CMake build

---
+++ build/lib/SDL2-2.0.20/include/SDL_revision.h
@@ -0,0 +1,6 @@
+#define SDL_REVISION "git@github.com:Runtime-Learner/rendering-engine.git@598f27d6ab59f0af92d2ea3684b850a26aef6f30"
+#define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
