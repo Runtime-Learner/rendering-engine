6c85d65b62c43dba292cbe67e24cb0499e79909b
Runtime-Learner <matthias15002@gmail.com>
2022-01-14 16:34:09 -0500

    fix import of SDL2 library again

--- build/lib/SDL2-2.0.20/include/SDL_revision.h
+++ build/lib/SDL2-2.0.20/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "git@github.com:Runtime-Learner/rendering-engine.git@598f27d6ab59f0af92d2ea3684b850a26aef6f30"
+#define SDL_REVISION "git@github.com:Runtime-Learner/rendering-engine.git@3bc0d1cb431544ce6a6e8e974f234172de8ed53b"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
