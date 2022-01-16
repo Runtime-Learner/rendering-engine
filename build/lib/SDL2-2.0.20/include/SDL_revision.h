e65e4a122e8eae19e1f80a23c795a6d578d3ac67
runtime-learner <matthias15002@gmail.com>
2022-01-15 19:23:19 -0500

    commit rest of files

--- build/lib/SDL2-2.0.20/include/SDL_revision.h
+++ build/lib/SDL2-2.0.20/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "git@github.com:Runtime-Learner/rendering-engine.git@fbfabf513e1a7be8b668bc8707372f369fac606c"
+#define SDL_REVISION "git@github.com:Runtime-Learner/rendering-engine.git@1c7f419af8a82b89f1a6c1a64d241e0461e84981"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
